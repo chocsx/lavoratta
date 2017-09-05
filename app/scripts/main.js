6c9d1d1bd10216c54186461f580523d1a7c527bd
Mateus Sobrinho <chocsx@hotmail.com>
2017-09-05 02:22:57 -0300

    some changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
 	$('.hamburger').click(function() {
 			 $('nav ul').toggleClass('open');
 			 $('.hamburger').toggleClass('is-active');
-	 });
+    });
+    $('.slider').slick({
+      dots: true
+    });
 })
